74e721401d6b2a5ce5b758ada1baa34ebb7acf26
Jake Surrett <30479707+Beizbol@users.noreply.github.com>
2023-02-10 20:42:31 -0500

    release 2

--- docs/chunks/Theme.astro_astro_type_script_index_0_lang.c8c31dd1.js
+++
@@ -1,1 +0,0 @@
-function l(){const e=localStorage.getItem("theme");if(e===null)return"darkTheme";const t=document.getElementById("themeSelect");t!==null&&(t.value=e)}l();
--- docs/hoisted.79475281.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/Theme.astro_astro_type_script_index_0_lang.c8c31dd1.js";
