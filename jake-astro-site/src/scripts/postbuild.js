31eb29d8edfd24741b4607f956978f0781e2725f
Jake Surrett <30479707+Beizbol@users.noreply.github.com>
2023-03-15 16:55:32 -0400

    fix postbuild

--- jake-astro-site/src/scripts/postbuild.js
+++ jake-astro-site/src/scripts/postbuild.js
@@ -1,23 +1,23 @@
 console.log("> starting postbuild");
 
-const fse = require('fs-extra');
-const path = require('path');
+import * as path from "path";
+import fs from "fs-extra";
 
 const srcDir = 'dist';
-const assetsDir = path.join(srcDir, 'assets');
 const destDir = path.join('..', 'docs');
+const assetsDir = path.join(destDir, 'assets');
 
 
 // delete assets folder to ensure new css
 console.log("> deleting old assets");
 
-fse.removeSync(assetsDir);
+fs.removeSync(assetsDir);
 
 // copy dist to docs with replacement
 console.log("> copying new build");
 
 // Copy back updated build to output folder
-fse.copySync(srcDir, destDir, {
+fs.copySync(srcDir, destDir, {
     overwrite: true
 }, (err) => {
     if (err) {
