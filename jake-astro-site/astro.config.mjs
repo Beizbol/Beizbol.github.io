0bc038411f874609136be0ef349d84f80ea4fbae
Jake Surrett <30479707+Beizbol@users.noreply.github.com>
2023-05-29 22:40:14 -0400

    bounce

--- jake-astro-site/astro.config.mjs
+++ jake-astro-site/astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import wasmPack from "vite-plugin-wasm-pack";
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
+import AstroPWA from '@vite-pwa/astro'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jakesite.ca',
-  integrations: [sitemap()],
+  integrations: [sitemap(), AstroPWA()],
   vite: {
     plugins:[wasmPack('./my-crate')],
   },
