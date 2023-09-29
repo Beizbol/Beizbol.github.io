a8dce6fdc0837e01bd43d179502283669937e5bc
Jake Surrett <30479707+Beizbol@users.noreply.github.com>
2023-09-29 15:27:56 -0400

    v8 update

--- docs/assets/hoisted.96cdb372.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.ed2dda3d.js";document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("agent");e.innerText+=" "+navigator.userAgent});
---
+++ docs/assets/hoisted.a592db89.js
@@ -0,0 +1,1 @@
+import"./hoisted.e207ed2d.js";document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("agent");e.innerText+=" "+navigator.userAgent});
