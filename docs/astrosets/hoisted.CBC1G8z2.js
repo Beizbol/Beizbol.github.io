56f3c25727e0ab435e7063448269205e0f48a237
Beizbol <30479707+Beizbol@users.noreply.github.com>
2024-02-21 16:06:53 -0500

    wordle fix

---
+++ docs/astrosets/hoisted.CBC1G8z2.js
@@ -0,0 +1,1 @@
+import"./hoisted.CmKvcf_A.js";document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("agent");e.innerText+=" "+navigator.userAgent});
--- docs/astrosets/hoisted.z6SiQz34.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.Yj7bA2Eb.js";document.addEventListener("DOMContentLoaded",()=>{let e=document.getElementById("agent");e.innerText+=" "+navigator.userAgent});
